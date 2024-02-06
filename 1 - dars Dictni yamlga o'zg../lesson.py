8013fe5d9b437ab6f5ee86ef468aea493ffe39d6
torabekov_08 <diyorbektorabekov08@gmail.com>
2024-02-06 11:46:30 +0500

    Initial commit

---
+++ 1 - dars Dictni yamlga o'zg../lesson.py
@@ -0,0 +1,30 @@
+# import yaml
+# data = {
+#     'Name': 'John Doe',
+#     'Position': 'DevOps Engineer',
+#     'Location': 'England',
+#     'Age': '26',
+#     'Experience': {'GitHub': 'Software Engineer', \
+#                    'Google': 'Technical Engineer', 'Linkedin': 'Data Analyst'},
+#     'Languages': {'Markup': ['HTML'], 'Programming' \
+#         : ['Python', 'JavaScript', 'Golang']}
+# }
+
+#Faylga yozish
+
+# with open("data.yaml","w") as file_yaml:
+#     yaml.dump(data, file_yaml)
+
+
+
+#Fayldan o'qish
+
+# with open("data.yaml","r") as file_yaml:
+#     data_yaml = yaml.safe_load(file_yaml)
+#     print(data_yaml)
+
+
+
+
+# data_yaml = yaml.dump(data)
+# print(data_yaml)
